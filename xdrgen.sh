0cb6ae749d8957dfb7f930d0cadc92c8c262dfc5
Chorn Sokun <csokun@users.noreply.github.com>
2021-11-24 16:00:30 +1100

    revert xdr.d.ts (#2)
    * revert xdr.d.ts
    
    * add xdr generate helper script

--- xdrgen.sh
+++ xdrgen.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 # generate xdr source code from xdr files
 # ------------------
-# gem install bundler:2.2.25
-# bundle install
-# rake xdr:generate
+# gem install bundler:2.2.25 && bundle install
+# yarn install && yarn run xdr
 # exit
+RAKE_IMAGE=rake:dev
+if [[ "$(docker images -q ${RAKE_IMAGE} 2> /dev/null)" == "" ]]; then
+cat <<EOF | docker build -t ${RAKE_IMAGE} -f - .
+FROM ruby:2.7
+
+SHELL ["/bin/bash", "-o", "pipefail", "-c"]
+
+RUN curl -sL https://deb.nodesource.com/setup_14.x | bash -\
+  && apt-get update -qq && apt-get install -qq --no-install-recommends \
+    nodejs \
+  && apt-get upgrade -qq \
+  && apt-get clean \
+  && rm -rf /var/lib/apt/lists/*\
+  && npm install -g yarn@1
+EOF
+fi
 docker run --rm -it \
     -v "$PWD":/working \
     -w /working \
     --entrypoint bash \
-    ruby:2.7.4-buster
+    $RAKE_IMAGE
 
 # reset file permission
 sudo chown $USER src/generated/stellar-xdr_generated.js
+
+# git clone https://github.com/stellar/dts-xdr
+# cd dts-xdr
+# npm install
+# OUT=stellar-xdr_generated.d.ts npx jscodeshift -t src/transform.js ../src/generated/stellar-xdr_generated.js
+# cp stellar-xdr_generated.d.ts ../types/xdr.d.ts
+# cd .. && rm -rf dts-xdr
+# yarn run prettier --write types/xdr.d.ts
+
